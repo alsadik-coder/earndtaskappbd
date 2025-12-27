b1c63d74f0c7b9ca37e2feb284b62c2bb4bfc658
ASD <mdalsadik1@gmail.com>
2025-12-27 13:59:40 +0300

    update

--- app/src/main/java/com/sadik/earntask/Fragments/ProfileFragment.java
+++ app/src/main/java/com/sadik/earntask/Fragments/ProfileFragment.java
@@ -27,33 +27,10 @@ public class ProfileFragment extends Fragment {
     ) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        TextView tvName = view.findViewById(R.id.tv_profile_name);
-        TextView tvEmail = view.findViewById(R.id.tv_profile_email);
-        MaterialButton btnLogout = view.findViewById(R.id.btn_logout);
+
 
         PrefManager prefManager = new PrefManager(requireContext());
 
-        // 🔹 Show name & email
-        tvName.setText(
-                requireContext()
-                        .getSharedPreferences("user_pref", 0)
-                        .getString("name", "User")
-        );
-
-        tvEmail.setText(
-                requireContext()
-                        .getSharedPreferences("user_pref", 0)
-                        .getString("email", "user@email.com")
-        );
-
-        // 🔹 Logout
-        btnLogout.setOnClickListener(v -> {
-            prefManager.logout();
-
-            Intent i = new Intent(requireContext(), LoginActivity.class);
-            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            startActivity(i);
-        });
 
         ///
 
